781d2acf06bf0e68cd423d404ad1c9399376b7db
TarasKing <ivanovtaras@live.ru>
2024-10-30 21:06:08 -0700

    helper functions

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 // helper functions
+const range = (start, end) =>
+  Array(end - start + 1)
+    .fill(start)
+    .map((element, index) => element + index);
+
+const charRange = (start, end) =>
+  range(start.charCodeAt(0), end.charCodeAt(0)).map((code) =>
+    String.fromCharCode(code)
+  );
+
+let x = charRange("A", "C");
+console.log(x);
